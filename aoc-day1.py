74c680eeb2b934a051858acfc4b80f95bc3668e1
Sal-Amin <93840411+Sal-Amin@users.noreply.github.com>
2021-12-01 12:33:53 -0700

    Update aoc-day1.py

--- aoc-day1.py
+++ aoc-day1.py
@@ -1,3 +1,13 @@
+import requests
+data_url = requests.get("https://adventofcode.com/2021/day/1/input")
+depth_reading = data_url.text
+depth_list = []
+for i, line in enumerate(depth_reading.split('\n')):
+  if line != "":
+    depth_list.append(int(line))
+
+# depth_list
+
 def depth_measure(li):
   """Takes a list of measures compares with the previous measure and yields if the depth increased or decreased"""
   pre_dep = 0
