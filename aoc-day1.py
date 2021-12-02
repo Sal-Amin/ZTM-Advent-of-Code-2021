c1b41904e11c82464396b4739e1f9d4f3a042fdc
Sal-Amin <93840411+Sal-Amin@users.noreply.github.com>
2021-12-01 18:02:20 -0700

    Update aoc-day1.py

--- aoc-day1.py
+++ aoc-day1.py
@@ -1,3 +1,4 @@
+# Load Data file
 import requests
 data_url = requests.get("https://adventofcode.com/2021/day/1/input")
 depth_reading = data_url.text
@@ -6,8 +7,7 @@ for i, line in enumerate(depth_reading.split('\n')):
   if line != "":
     depth_list.append(int(line))
 
-# depth_list
-
+# Part- 1:
 def depth_measure(li):
   """Takes a list of measures compares with the previous measure and yields if the depth increased or decreased"""
   pre_dep = 0
@@ -28,3 +28,26 @@ def depth_measure(li):
   return dep_measure.count("increased")
 
 depth_measure(depth_list)
+
+# Part- 2:
+def sliding_window(li):
+  cluster_dep = []
+  cluster = []
+  tenp_clus = 0
+  for i in range(len(li)):
+    if i+2 < len(li):
+      temp_clus = (li[i]+li[i+1]+li[i+2])
+      cluster.append(temp_clus)
+  for i in range(len(cluster)):
+    if i == 0:
+      cluster_dep.append("N/A - no previous measurement")
+    elif cluster[i]> cluster[i-1]:
+      cluster_dep.append("increased")
+    elif cluster[i]> cluster[i-1]:
+      cluster_dep.append("decreased")
+    elif cluster[i] == cluster[i-1]:
+      cluster_dep.append("no change")
+  return cluster_dep.count("increased")
+
+sliding_window(depth_list)
+
